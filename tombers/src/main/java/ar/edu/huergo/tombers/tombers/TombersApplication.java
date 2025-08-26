254f3598dfa0f65f040533a049f5b03ac0d2e9a9
EzeAbelle <eabelle@alumno.huergo.edu.ar>
2025-08-26 20:12:22 -0300

    inicio de proyecto

---
+++ tombers/src/main/java/ar/edu/huergo/tombers/tombers/TombersApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.huergo.tombers.tombers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombersApplication.class, args);
+	}
+
+}
---
+++ tombers/src/test/java/ar/edu/huergo/tombers/tombers/TombersApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.huergo.tombers.tombers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TombersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
